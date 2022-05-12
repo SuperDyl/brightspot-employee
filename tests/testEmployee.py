4b842704af307cd5ac8b2f4b8273143c384849d4
Dylan <SuperDyl19@gmail.com>
2022-05-11 19:35:13 -0600

    Updated professor tests (and made it employee tests instead)

--- tests/testEmployee.py
+++ tests/testEmployee.py
@@ -1,6 +1,6 @@
-from src.professor import Professor
+from src.professor import Employee, EmployeeProcessor
 
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 import unittest
 
 
@@ -9,21 +9,23 @@ RELIGION_DIR_URL = 'https://religion.byu.edu/directory'
 
 class TestProfessor(unittest.TestCase):
     def setUp(self) -> None:
-        self.all_profs = Professor.from_website()
+        Employee.processor = EmployeeProcessor('PromoVerticalImage')
+        self.all_profs = Employee.from_website(RELIGION_DIR_URL)
 
     def test_csv(self):
         all_profs = self.all_profs
 
         with NamedTemporaryFile('w+') as file:
             a = '\n'.join(tuple(str(x) for x in all_profs))
-            Professor.to_csv(file.name, all_profs)
+            Employee.to_csv(file.name, all_profs)
 
-            csv = Professor.from_csv(file.name)
+            csv = Employee.from_csv(file.name)
             b = '\n'.join(tuple(str(x) for x in csv))
         self.assertEqual(a, b)
 
     def test_download_photo(self):
-        self.all_profs[0].download_photo('pics')
+        with TemporaryDirectory() as temp_dir:
+            self.all_profs[0].download_photo(temp_dir)
 
 
 if __name__ == '__main__':
